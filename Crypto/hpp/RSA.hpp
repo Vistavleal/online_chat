e232d1b6f8bdc61a8f07c02f1a063c2430fdad99
Vellen <vistavlealbox@gmail.com>
2021-05-31 21:37:07 +0300

    Система шифрования RSA

---
+++ Crypto/hpp/RSA.hpp
@@ -0,0 +1,40 @@
+#ifndef RSA_HPP
+#define RSA_HPP
+
+#include <string>
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <string.h>
+#include <limits>
+#include <limits.h>
+
+struct public_key
+{
+        long long modules;
+        long long exponental;
+};
+
+struct private_key
+{
+        long long modules;
+        long long exponental;
+};
+
+
+void RSA_gen_keys(public_key*, private_key*);
+std::vector<long long> RSA_encrypt(public_key*, char*);
+std::vector<char> RSA_decrypt(private_key*, char*, std::vector<long long>);
+long long extented_GDC(long long, long long, int&, int&);
+long long gdc(long long, long long);
+long long invmod(long long, long long);
+unsigned long long degree (int, long long);
+int* cmprss(int);
+unsigned long long sqr(unsigned long long);
+
+#endif 
